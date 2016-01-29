ff54a7bd70c8b5187e6b83a68541b643999fdda6
Ashes1Away <Ashes1Away@gmail.com>
2016-01-29 23:02:40 +0000

    Main Proj bisnatches

---
+++ New Unity Project 1/Assets/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    private float _damageModifier;
+    private float _speedModifier;
+    private float _armourModifier;
+    private float _hpRegenModifier;
+
+    private float _health;
+
+    public float damageModifier { get { return _damageModifier; } set { _damageModifier = value; } }
+    public float speedModifier { get { return _speedModifier; } set { _speedModifier = value; } }
+    public float armourModifier { get { return _armourModifier; } set { _armourModifier = value; } }
+    public float hpRegenModifier { get { return _hpRegenModifier; } set { _hpRegenModifier = value; } }
+
+    public float health { get { return _health; } }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        HpRegen();
+	}
+
+    private void HpRegen()
+    {
+        _health += hpRegenModifier * Time.deltaTime;
+    }
+}
