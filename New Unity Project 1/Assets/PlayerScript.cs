0b75cac869fe2fcf5d9f54f184ad2f5f7c265cb2
Ashes1Away <Ashes1Away@gmail.com>
2016-01-30 03:14:07 +0000

    Deleted Base.Unity and fully modified the Player script

--- New Unity Project 1/Assets/PlayerScript.cs
+++ New Unity Project 1/Assets/PlayerScript.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-    private float _damageModifier = 5;
-    private float _speedModifier = 100;
-    private float _armourModifier = 100;
-    private float _hpRegenModifier = 5;
+    private float _damageModifier = 1f;
+    private float _speedModifier = 1f;
+    private float _armourModifier = 1f;
+    private float _hpRegenModifier = 1f;
 
     public bool freezeControls; //For Liam, cos he's weird
 
-    private float _health;
+    private float _health = 100f;
+    private float _damage = 5f;
+    private float _speed = 25f;
 
     public float damageModifier { get { return _damageModifier; } set { _damageModifier = value; } }
     public float speedModifier { get { return _speedModifier; } set { _speedModifier = value; } }
@@ -20,13 +22,28 @@ public class PlayerScript : MonoBehaviour {
     public float health { get { return _health; } }
 
     Rigidbody2D rigidbody;
+    Collider2D collider;
 
 
 	// Use this for initialization
 	void Start () {
+
         rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
 	}
 	
+    void Update()
+    {
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject e in allEnemies)
+        {
+            if (collider.IsTouching(e.GetComponent<Collider2D>())&&Input.GetButtonDown("Fire1"))
+            {
+                e.GetComponent<AIScript>().TakeDamage(damageModifier * _damage);
+            }
+        }
+    }
+
 	// Update is called once per frame
 	void FixedUpdate () {
         HpRegen();
@@ -43,9 +60,14 @@ public class PlayerScript : MonoBehaviour {
         if (!freezeControls) //When freezeControls is true do not allow controls
         {
             rigidbody.velocity = Vector2.zero;
-            Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal") * speedModifier * Time.deltaTime, Input.GetAxisRaw("Vertical") * speedModifier * Time.deltaTime);
+            Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal") * _speed * speedModifier * Time.deltaTime, Input.GetAxisRaw("Vertical") * _speed * speedModifier * Time.deltaTime);
             rigidbody.velocity = movement;
         }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _health -= damage * armourModifier;
     }
 
 
